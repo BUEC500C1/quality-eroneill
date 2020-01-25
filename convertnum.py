53f985bc6eb71dbc12f9d3ed1fd0bf74167158bc
eroneill <e.12.oneill@gmail.com>
2020-01-25 17:13:05 -0500

    Adding first file to test

---
+++ convertnum.py
@@ -0,0 +1,28 @@
+# EC500 Hw1
+# Copyright 2020  Erin K O'Neill erinkate@bu.edu
+
+# convert arabic numerals to roman 
+# first pass no edge cases/error checking 
+
+import string 
+
+
+def convertnum(int x):
+    """convert the integer x into a string representation of x in roman numerals"""
+    
+    # base 6 is 0-5 in each position so divide by 6, save % and keep rolling 
+    stringsix = ""
+    if x < 6:
+        stringsix = str(x%6)
+        return stringsix
+    while x > 0 : 
+        firstsix = int(x%6)
+        x = (x-firstsix)/6
+        stringsix = str(firstsix) + stringsix
+    return str(stringsix)
+
+def main():
+    print(convertnum(48))
+
+if __name__ == '__main__':
+    main()
---
+++ first_test.py
@@ -0,0 +1,11 @@
+# simple test - first_test.py
+
+import pytest
+from convertnum.py import convertnum
+
+def f():
+    raise SystemExit(1)
+
+def test_first():
+    with pytest.raises(SystemExit):
+        f()
