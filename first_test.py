08149163d27e8ede09040d3aa793b6aea6f3564f
eroneill <e.12.oneill@gmail.com>
2020-01-25 18:42:44 -0500

    Fixed type

--- first_test.py
+++ first_test.py
@@ -10,4 +10,4 @@ def test_first():
     testinp = [111, 40, 55, 2, 3999, 4000, 30.5, 2421, 789]
     testout = ['CXI', 'XL', 'LV', 'II', 'MMMCMXCIX', 'too large', 'not int', 'MMCDXXI', 'DCCLXXXIX']
     for val in range(len(testinp)):
-        assert convertnum(testinp[val]) == testout[val]git 
+        assert convertnum(testinp[val]) == testout[val]
