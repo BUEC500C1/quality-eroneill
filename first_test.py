a9c56c2e2542af1ce32e0a927b170108f1ee40f5
eroneill <e.12.oneill@gmail.com>
2020-01-25 18:40:59 -0500

    updated logic to catch 9 values

--- first_test.py
+++ first_test.py
@@ -7,7 +7,7 @@ def f():
     raise SystemExit(1)
 
 def test_first():
-	testinp = [111, 40, 55, 2]
-	testout = ['CXI', 'XL', 'LV', 'II']
-	for val in range(len(testinp)):
-		assert convertnum(testinp[val]) == testout[val]
+    testinp = [111, 40, 55, 2, 3999, 4000, 30.5, 2421, 789]
+    testout = ['CXI', 'XL', 'LV', 'II', 'MMMCMXCIX', 'too large', 'not int', 'MMCDXXI', 'DCCLXXXIX']
+    for val in range(len(testinp)):
+        assert convertnum(testinp[val]) == testout[val]git 
