58f6e41c980ed37fbafde3ff71df49ab6e62057a
eroneill <e.12.oneill@gmail.com>
2020-01-25 17:58:41 -0500

    updating test cases

--- first_test.py
+++ first_test.py
@@ -1,11 +1,16 @@
 # simple test - first_test.py
 
 import pytest
-# from convertnum.py import convertnum
+from convertnum import convertnum
 
 def f():
     raise SystemExit(1)
 
 def test_first():
+	testinp = [111, 40, 55, 2]
+	testout = ['CXI', 'XL', 'LV', 'II']
+	for val in range(len(testinp)):
+		assert convertnum(testinp[val]) == testout[val]
+    
     with pytest.raises(SystemExit):
         f()
